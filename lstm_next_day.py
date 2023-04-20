0894dd11109f6da1d34c6c11c32d4423455d6617
bszek213 <bszekely@nevada.unr.edu>
2023-04-19 19:52:06 -0700

    ADD: keras model to predict crypto price change tomorrow

---
+++ lstm_next_day.py
@@ -0,0 +1,133 @@
+#Predict the next day price - regression like
+
+import pandas as pd
+import numpy as np
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout #, LeakyReLU, BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
+from sys import argv
+import os
+from tensorflow.keras.models import load_model
+# Define the input and output data
+# def create_dataset(dataset, look_back=1):
+#     X, y = [], []
+#     for i in range(len(dataset) - look_back):
+#         X.append(dataset[i:i + look_back, :-1])
+#         y.append(dataset[i + look_back, -1])
+#     print(X)
+#     input()
+#     X = np.array(X).reshape(-1, look_back, 1)
+#     y = np.array(y)
+#     return X, y
+
+def prepare_data(data, n_steps):
+    X, y = [], []
+    for i in range(len(data)):
+        end_ix = i + n_steps
+        if end_ix >= len(data):
+            break
+        seq_x, seq_y = data[i:end_ix], data[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+class nextDay():
+    def __init__(self):
+        print('instantiate nextDay class object')
+        self.crypto = argv[1]
+        self.n_steps = 60 #Increasing the sequence length allows the model to capture more long-term dependencies in the data, but can also increase the computational cost of training the model.
+    # prepare the data for input into the LSTM model
+    def read_data(self):
+        # Download BTC data from Yahoo Finance
+        name = self.crypto + '-USD'
+        self.data = yf.download(name, period='max')
+    def get_percent_change(self):
+        # Prepare the data
+        self.data = self.data[['Close']]
+        self.data['pct_change'] = self.data['Close'].pct_change()
+        self.data = self.data.dropna()
+    def normalize_data(self):
+        # Normalize the data
+        self.scaler = MinMaxScaler()
+        self.data_scaled = self.scaler.fit_transform(self.data['pct_change'].values.reshape(-1,1))
+    def split_data(self):
+        # Split the data into training and test sets
+        # train_size = int(len(self.data) * 0.75)
+        # self.train_data = self.data_scaled[:train_size]
+        # self.test_data = self.data_scaled[train_size:]
+        split_idx = int(len(self.X) * 0.8)
+        self.X_train, self.X_test = self.X[:split_idx], self.X[split_idx:]
+        self.y_train, self.y_test = self.y[:split_idx], self.y[split_idx:]
+        print('==============================================')
+        print(f'length of traininig set: {len(self.X_train)}')
+        print(f'length of test set: {len(self.X_test)}')
+        print('==============================================')
+    def create_seq(self):
+        # data = self.data['pct_change'].values
+        data = self.data_scaled
+        self.X, self.y = prepare_data(data, self.n_steps)
+    def algo(self):
+        if os.path.exists('LSTM_next_day_model.h5'):
+            self.model = load_model('LSTM_next_day_model.h5')
+        else:
+            # Define the deep neural network model
+            self.model = Sequential()
+            self.model.add(LSTM(units=32, activation='relu', return_sequences=True, input_shape=(self.n_steps, 1)))
+            self.model.add(Dropout(0.2))
+            self.model.add(LSTM(units=16, activation='relu', return_sequences=True, input_shape=(self.n_steps, 1)))
+            self.model.add(Dropout(0.2))
+            self.model.add(LSTM(units=8, activation='relu', return_sequences=True, input_shape=(self.n_steps, 1)))
+            # self.model.add(Dropout(0.2))
+            # self.model.add(LSTM(units=8, activation='relu'))
+            self.model.add(Dropout(0.2))
+            self.model.add(Dense(units=1, activation='linear'))
+            self.model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001))
+            self.model.summary()
+            # Train the model
+            #run this to see the tensorBoard: tensorboard --logdir=./logs
+            tensorboard_callback = TensorBoard(log_dir="./logs")
+            self.model.fit(self.X_train, self.y_train, epochs=50, 
+                        validation_data=(self.X_test, self.y_test),
+                        batch_size=self.n_steps,verbose=1,
+                        callbacks=[tensorboard_callback]
+                        )
+            self.model.save('LSTM_next_day_model.h5')
+        # # Make predictions
+        # y_pred = self.model.predict(self.X_test)
+        # y_pred_unscaled = self.scaler.inverse_transform(np.concatenate((self.X_test[:, -1, :-1], y_pred.reshape(-1, 1)), axis=1))[:, -1]
+        # y_test_unscaled = self.scaler.inverse_transform(np.concatenate((self.X_test[:, -1, :-1], self.y_test.reshape(-1, 1)), axis=1))[:, -1]
+
+        # Print the predicted percent change for the next day
+        # print(f'Predicted BTC percentage change for tomorrow: {y_pred_unscaled[-1]:.4f}')
+    def predict_future(self):
+        # # use the model to predict the next 7 days of prices
+        # last_days = self.data['pct_change'].tail(self.n_steps).values.reshape(-1, 1)
+        # last_days = self.scaler.transform(last_days)
+        # last_days = last_days.reshape(1, self.n_steps, 1)
+        # pred = self.model.predict(last_days) #
+        # print(pred[0].shape)
+        # print(pred[0][0])
+        # # print the predicted prices for tomorrow days
+        # print('Predicted percent price for tomorrow: ', self.scaler.inverse_transform(pred[0][0].reshape(-1, 1)))
+        last_days = self.data['pct_change'].tail(self.n_steps).values.reshape(-1, 1)
+        last_days = self.scaler.transform(last_days)
+        last_days = last_days.reshape(1, self.n_steps, 1)
+        pred = self.model.predict(last_days)
+        # extract the predicted price
+        predicted_price = self.scaler.inverse_transform(pred[0])[0][0]
+        print('Predicted price for tomorrow: ', predicted_price)
+    def run_analysis(self):
+        self.read_data()
+        self.get_percent_change()
+        self.normalize_data()
+        self.create_seq()
+        self.split_data()
+        self.algo()
+        self.predict_future()
+def main():
+    nextDay().run_analysis()
+if __name__ == "__main__":
+    main()
